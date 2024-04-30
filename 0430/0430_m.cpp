c9ba404ea121da4563933d929429ef233567a5d9
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-30 15:15:53 +0900

    ad

--- 0430/0430_m.cpp
+++ 0430/0430_m.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 int main()
 {
-	string s = "Police say two people are suspected of trying to create a shortcut for their construction work.The two have been detained and the case is under further investigation.The 38 - year - old man and 55 - year - old woman were working near the affected area, the 32nd Great Wall.";
+	string s = "Police say two people are suspected of trying to create a shortcut for their construction work.The two have been detained and the case is under further investigation.The 38-year-old man and 55-year-old woman were working near the affected area, the 32nd Great Wall.";
 	
 	cout << "문자열 길이 : ";
 	cout << s.length() << endl;
 
-	cout << "100번째 문자 출력 : ";
-	cout << s.at(100) << endl;
+	int n;
+	cout << "몇 번쨰 문자 출력 : ";
+	cin >> n;
+	cout << n << " 번째 문자는 " << s[n - 1] << endl;
 
 	cout << "two 라는 문자 처음 index : ";
 	cout << s.find("two") << endl;
