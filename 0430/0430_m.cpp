6fd5daf3ae969c7c0b334882cefce0bce94a5ccd
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-30 15:07:44 +0900

    dd

---
+++ 0430/0430_m.cpp
@@ -0,0 +1,22 @@
+//실습1 string
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s = "Police say two people are suspected of trying to create a shortcut for their construction work.The two have been detained and the case is under further investigation.The 38 - year - old man and 55 - year - old woman were working near the affected area, the 32nd Great Wall.";
+	
+	cout << "문자열 길이 : ";
+	cout << s.length() << endl;
+
+	cout << "100번째 문자 출력 : ";
+	cout << s.at(100) << endl;
+
+	cout << "two 라는 문자 처음 index : ";
+	cout << s.find("two") << endl;
+
+	cout << "two 라는 문자 두번째 index : ";
+	cout << s.find("two", s.find("two")+1);
+}
